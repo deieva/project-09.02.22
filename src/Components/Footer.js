fda90dd3f5b07f71e19fe3232939d4469f9abd29
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-04-11 13:53:40 +0300

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,24 +6,24 @@ function Footer() {
             <div>
                 <div className="footer">
                     <section>
-                        <h3>XRIGADE</h3>
+                        <h3 className="footer-section">XRIGADE</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
                     </section>
                     <section>
-                        <h3>LINKS</h3>
+                        <h3 className="footer-section">LINKS</h3>
                         <a href="#">Home</a>
                         <a href="#">About Us</a>
                         <a href="#">Blog</a>
                         <a href="#">Contact Us</a>
                     </section>
                     <section>
-                        <h3>OTHER</h3>
+                        <h3 className="footer-section">OTHER</h3>
                         <a href="#">Privacy Policy</a>
                         <a href="#">Terms and Conditions</a>
                         <a href="#">Cookie Policy</a>
                     </section>
                     <section>
-                        <h3>ANY QUESTIONS?</h3>
+                        <h3 className="footer-section">ANY QUESTIONS?</h3>
                         <a href="emailto:support@websitename.com"><span><i className="fa fa-envelope"></i></span><span>support@websitename.com</span></a>
                         <a href="tel:xxxxxxxxx"><span><i className="fa fa-phone"></i></span><span>(+x)-xxx-xxxx-xxxxx</span></a>
                     </section>
