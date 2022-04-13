d8fb6db1d145a27dc2908b69be9f4d6e21898a8b
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-04-13 17:34:31 +0300

    update login

--- src/Views/Cart.js
+++ src/Views/Cart.js
@@ -3,7 +3,7 @@ function Cart() {
 
     return (
         <div>
-
+           
         </div>
     )
 }
--- src/Views/Login.js
+++ src/Views/Login.js
@@ -3,6 +3,28 @@ function Login() {
 
     return (
         <div>
+            <div className="container">
+                <h1>Login</h1>
+                <form action="">
+                    <div className="box">
+                        <input type="text" placeholder="Username" required maxlength="25" />
+                    </div>
+                    <div className="box">
+                        <input type="password" placeholder="Password" required maxlength="15" />
+                    </div>
+                    <input type="submit" className="btn" value="Login" />
+                </form>
+                <a href="#" className="b1">Forget Password?</a>
+                <a href="#" className="b2">Create Account</a>
+                <br /><br />
+                <hr />
+                <p>Or Login With</p>
+                <div className="icon">
+                    <i className="fab fa-github fa-2x"></i>
+                    <i className="fab fa-google fa-2x"></i>
+                    <i className="fab fa-facebook-square fa-2x"></i>
+                </div>
+            </div>
 
         </div>
     )
