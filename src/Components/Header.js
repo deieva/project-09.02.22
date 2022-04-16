369e868f2f997186292086335140eaf3f165e088
vaivodeieva <87878157+deieva@users.noreply.github.com>
2022-04-16 21:51:54 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,27 +21,27 @@ function Header() {
             <header id="container">
                 <div id="nav-bar">
                     <div id="nav-belt">
-                        <div class="nav-left">
+                        <div className="nav-left">
                             <div id="nav-logo">
                                 <a href="/">MOUNTAINN</a>
                             </div>
                         </div>
-                        {/* <div class="nav-fill">
+                        {/* <div className="nav-fill">
                             <div id="nav-search">
                                 <form id="nav-search-bar-form">
-                                    <div class="nav-left">
+                                    <div className="nav-left">
                                         <a href="">ALL</a>
                                     </div>
-                                    <div class="nav-fill">
+                                    <div className="nav-fill">
                                         <input type="text" />
                                     </div>
-                                    <div class="nav-right">
+                                    <div className="nav-right">
                                         <button type="submit">Search</button>
                                     </div>
                                 </form>
                             </div>
                         </div> */}
-                        <div class="nav-right">
+                        <div className="nav-right">
                             <div id="nav-tools">
                                 <a href="/services">Services</a>
                                 <a href="/login">Log in</a>
